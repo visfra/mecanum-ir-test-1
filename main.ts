a561c93aa1a0f53fecaf96dc4bb3a734d1e5f98c
visfra <visfra13@gmail.com>
2025-02-23 19:37:27 +0100

    dal telecomando destra sinistra avanti indietro

--- main.ts
+++ main.ts
@@ -8,20 +8,26 @@ mecanumRobotV2.state()
 // - down: 21
 basic.forever(function () {
     ir_button = irRemote.returnIrButton()
-    if (ir_button == 68) {
-        basic.showString("L")
-        mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, 30)
-    } else if (ir_button == 67) {
+    if (ir_button == 67) {
         basic.showString("R")
+        mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, 30)
+        mecanumRobotV2.Motor(LR.Lower_left, MD.Forward, 30)
+    } else if (ir_button == 68) {
+        basic.showString("L")
         mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, 30)
+        mecanumRobotV2.Motor(LR.Lower_right, MD.Forward, 30)
     } else if (ir_button == 70) {
         basic.showString("U")
         mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, 30)
         mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, 30)
+        mecanumRobotV2.Motor(LR.Lower_right, MD.Forward, 30)
+        mecanumRobotV2.Motor(LR.Lower_left, MD.Forward, 30)
     } else if (ir_button == 21) {
         basic.showString("D")
         mecanumRobotV2.Motor(LR.Lower_left, MD.Forward, 10)
         mecanumRobotV2.Motor(LR.Lower_right, MD.Forward, 10)
+        mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, 10)
+        mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, 10)
     } else {
         basic.clearScreen()
         mecanumRobotV2.state()
