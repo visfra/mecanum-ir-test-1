1f709aa45b0a2ede1b2a937c28788542399e6882
visfra <visfra13@gmail.com>
2025-02-23 19:56:50 +0100

    risolvimento problemi

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+input.onPinPressed(TouchPin.P0, function () {
+    irRemote.connectInfrared(DigitalPin.P0)
+    for (let index = 0; index < 2; index++) {
+        mecanumRobotV2.setLed(LedCount.Left, LedState.ON)
+    }
+})
 let ir_button = 0
 irRemote.connectInfrared(DigitalPin.P0)
 mecanumRobotV2.state()
@@ -10,12 +16,12 @@ basic.forever(function () {
     ir_button = irRemote.returnIrButton()
     if (ir_button == 67) {
         basic.showString("R")
-        mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, 30)
-        mecanumRobotV2.Motor(LR.Lower_left, MD.Forward, 30)
-    } else if (ir_button == 68) {
-        basic.showString("L")
         mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, 30)
         mecanumRobotV2.Motor(LR.Lower_right, MD.Forward, 30)
+    } else if (ir_button == 68) {
+        basic.showString("L")
+        mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, 30)
+        mecanumRobotV2.Motor(LR.Lower_left, MD.Forward, 30)
     } else if (ir_button == 70) {
         basic.showString("U")
         mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, 30)
@@ -24,10 +30,10 @@ basic.forever(function () {
         mecanumRobotV2.Motor(LR.Lower_left, MD.Forward, 30)
     } else if (ir_button == 21) {
         basic.showString("D")
-        mecanumRobotV2.Motor(LR.Lower_left, MD.Forward, 10)
-        mecanumRobotV2.Motor(LR.Lower_right, MD.Forward, 10)
-        mecanumRobotV2.Motor(LR.Upper_right, MD.Forward, 10)
-        mecanumRobotV2.Motor(LR.Upper_left, MD.Forward, 10)
+        mecanumRobotV2.Motor(LR.Lower_left, MD.Back, 30)
+        mecanumRobotV2.Motor(LR.Lower_right, MD.Back, 30)
+        mecanumRobotV2.Motor(LR.Upper_right, MD.Back, 30)
+        mecanumRobotV2.Motor(LR.Upper_left, MD.Back, 30)
     } else {
         basic.clearScreen()
         mecanumRobotV2.state()
